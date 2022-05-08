39530a1403446324f9276d53f08c4bf21c879695
JusticeCaban <nyjustice3@gmail.com>
2022-05-07 23:46:16 -0500

    Created view that accepts a URL and assigns a random id to it and a view that accepts a short url and returns the original.

--- urlShortener_app/views.py
+++ urlShortener_app/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+import random, string
+from django.shortcuts import redirect
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import urlShortener
+from .serializers import urlShortenerSerializer
 
 # Create your views here.
+@api_view(['POST'])
+def makeShortURL(request):
+    data = request.data
+    alphabet = string.ascii_lowercase + string.digits
+    shorturl = ''.join(random.choices(alphabet, k=8))
+    try:
+        urlShortener.objects.create(
+            longurl = data.longurl,
+            shorturl = shorturl
+        )
+    except:
+        shorturl = ''.join(random.choices(alphabet, k=8))
+        urlShortener.objects.create(
+            longurl = data.longurl,
+            shorturl = shorturl
+        )
+    longurl = data['longurl']
+    shorturl = "http://localhost:8000/"+shorturl
+    
+    return Response({'longurl': longurl, 'shorturl': shorturl})
+
+def redirectUrl(request, shorturl):
+    try:
+        shortLink = urlShortener.objects.get(shorturl=shorturl)
+    except urlShortener.DoesNotExist:
+        shortLink = None
+
+    if shortLink is not None:
+        return Response(shortLink)

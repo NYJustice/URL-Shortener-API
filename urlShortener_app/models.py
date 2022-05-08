f77d0517cac590964c9f9b95655e151d39edd0b2
JusticeCaban <nyjustice3@gmail.com>
2022-05-07 23:34:28 -0500

    Created urlShortener class with field for long and short URL's

--- urlShortener_app/models.py
+++ urlShortener_app/models.py
@@ -1,3 +1,10 @@
+from operator import truediv
 from django.db import models
 
 # Create your models here.
+class urlShortener(models.Model):
+    long_url = models.URLField(max_length=300, unique=True)
+    short_url = models.URLField(max_length=300, unique=True)
+    
+    def __str__(self):
+        return self.short_url
